28f3babc7b6cac4f6387d53a9e56ecfd949f6349
ille <41062288+illenium3812@users.noreply.github.com>
2022-04-19 21:15:10 +0500

    Add files via upload

---
+++ YT-bulk-download.py
@@ -0,0 +1,60 @@
+# importing the module 
+import sys
+import os
+import pandas as pd
+from moviepy.editor import *
+import pytube 
+
+def read_csv(PATH,label):
+    try:
+        df = pd.read_csv(PATH)
+    except:
+        print('ERROR :File not found, check file path again')
+      
+    lables = df['label'].tolist()
+    youtube_ids = df['youtube_id'].tolist()
+    time_start = df['time_start'].tolist()
+    time_end = df['time_end'].tolist()
+    l = []
+
+    for i in range(len(lables)):
+        if label in lables[i]:
+            l.append(lables[i] + "," + f'https://www.youtube.com/watch?v={youtube_ids[i]}' + "," + str(time_start[i]) + "," + str(time_end[i]))    
+    return l
+
+def clip(path,file_name,start_time,end_time):
+    video = VideoFileClip(f'{path}\\{file_name}.mp4').subclip(start_time,end_time)
+    frames = video.fps
+    video.write_videofile(f'{path}\\clipped\\{file_name}.mp4',fps=frames)
+
+def remove_special_characters(name):
+    l = ["\\","/","*",":","|","?","<",">",'"']
+    for letter in l:
+        if letter in name:
+            name = name.replace(letter,"")
+    return name
+
+def downloadVideo(urls,label):
+    cwd = os.getcwd() 
+    PATH = f'{cwd}\\{label} videos'
+    for url in urls:
+        try:
+            youtube = pytube.YouTube(url.split(',')[1])
+            video = youtube.streams.get_highest_resolution()
+            print(f'Downloading {video.title} | size = {video.filesize/1000000} mb')
+            video.download(filename = f'{PATH}\\{remove_special_characters(video.title)}.mp4')
+            clip(PATH,remove_special_characters(video.title),int(url.split(',')[2]),int(url.split(',')[3]))
+        except Exception as e: print(e)
+    
+if __name__ == "__main__":
+    LABEL = sys.argv[1]
+    try:
+        os.mkdir(f'{LABEL} videos')
+    except:
+        pass
+    try:
+        os.mkdir(f'{LABEL} videos\\clipped')
+    except:
+        pass
+    urls = read_csv('train.csv',LABEL)
+    downloadVideo(urls,LABEL)
